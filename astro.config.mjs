6134542c5f5780d9725358049ffa931e0abbd118
Pratikkk <kenipratik9@gmail.com>
2023-06-20 11:45:01 +0530

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Pratikkk.github.io',
   base: '/agency-site',
   output: "server",
-  adapter: netlify()
+
 });
